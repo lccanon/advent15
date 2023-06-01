23ac8e7e2f0631a5aee7b52ca758f84f468f328a
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-01 16:09:25 +0200

    reddit info

--- 1.R
+++ 1.R
@@ -1,6 +1,15 @@
+# https://www.reddit.com/r/adventofcode/wiki/archives/solution_megathreads/2015/
+
+# https://www.reddit.com/r/programming/comments/3uyl7s/daily_programming_puzzles_at_advent_of_code/
+
 library(tidyverse)
 
 paran <- (readLines("input1") %>%
             str_split(., ""))[[1]]
 print(sum(paran == "(") - sum(paran == ")"))
 print(which(cumsum((paran == "(") * 2 - 1) == -1)[1])
+
+# Better read
+
+strsplit(scan("input1","character"),"")[[1]] %>%
+  match(c(")","","(")) - 2
