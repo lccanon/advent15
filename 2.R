7f1376ff656e68e5f21755cd9a24ee81e264b4e9
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-02 15:32:31 +0200

    Minor fix

--- 2.R
+++ 2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-gift <- read.table("input2", sep = "x", col.names = c("l", "w", "h")) %>%
-  as_tibble() %>%
+gift <- read_delim("input2", delim = "x", col_names = c("l", "w", "h")) %>%
   mutate(paper = 2 * (l * w + l * h + w * h) + pmin(l * w, l * h, w * h),
          ribbon = l * w * h + 2 * pmin(l + w, l + h, w + h)) %>%
   summarise(paper = sum(paper), ribbon = sum(ribbon))
+print(gift)
