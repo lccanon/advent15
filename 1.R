8b9efc4d1de9198cefb8f2031201ba8d9be62db2
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-03 13:48:24 +0200

    Day 3

--- 1.R
+++ 1.R
@@ -7,7 +7,7 @@ setwd("~/Documents/divers/advent15")
 library(tidyverse)
 
 paran <- (readLines("input1") %>%
-            str_split(., ""))[[1]]
+            str_split(""))[[1]]
 print(sum(paran == "(") - sum(paran == ")"))
 print(which(cumsum((paran == "(") * 2 - 1) == -1)[1])
 
