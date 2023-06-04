4a5846a933a7404bc5c3e83411a84ae1043cfab1
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-04 12:17:38 +0200

    Day 4

---
+++ 4.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(digest)
+
+key <- "bgvyzdsv"
+# key <- "abcdef"
+leading_zero <- 6
+
+i <- 1
+while (TRUE) {
+  md5 <- digest(str_c(key, i), algo = "md5", serialize = FALSE)
+  if (str_sub(md5, 0, leading_zero) == str_flatten(rep("0", leading_zero)))
+    break
+  i <- i + 1
+}
+print(i)
