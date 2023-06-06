c148ace3a32e2919e5e092030c0f37056f5f8503
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-06 13:58:46 +0200

    Day 6

---
+++ 6.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+insts <- readLines("input6") %>%
+  str_replace("turn ", "") %>%
+  str_replace(" through ", ",") %>%
+  str_replace(" ", ",") %>%
+  tibble() %>%
+  separate(sep = ",", col = ".", into = c("action", "x1", "y1", "x2", "y2"))
+
+n <- 1000
+lights <- matrix(0, nrow = n, ncol = n)
+for (i in 1:nrow(insts)) {
+  index_i <- unlist(insts[i,"x1"]):unlist(insts[i,"x2"]) + 1
+  index_j <- unlist(insts[i,"y1"]):unlist(insts[i,"y2"]) + 1
+  if (insts[i,"action"] == "on")
+    lights[index_i,index_j] <- lights[index_i,index_j] + 1
+  else if (insts[i,"action"] == "off")
+    lights[index_i,index_j] <- lights[index_i,index_j] - 1
+  else if (insts[i,"action"] == "toggle")
+    lights[index_i,index_j] <- lights[index_i,index_j] + 2
+  else
+    print("Problem")
+  lights[lights < 0] <- 0
+}
+print(sum(lights))
