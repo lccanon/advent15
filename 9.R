bb6447e9eb00c33d5abdb07d1a5634b517779950
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-10 12:25:29 +0200

    Visu

--- 9.R
+++ 9.R
@@ -4,7 +4,7 @@ dd <- read_delim("input9", delim = " ",
                  col_names = c("src", "X1", "dst", "X2", "dist")) %>%
   select(-X1, -X2)
 city <- factor(unique(c(dd$src, dd$dst)))
-dd <- dd %>%
+distances <- dd %>%
   mutate(tmp = dst) %>%
   mutate(dst = src) %>%
   mutate(src = tmp) %>%
@@ -13,7 +13,7 @@ dd <- dd %>%
   mutate(dst = factor(dst, city)) %>%
   rbind(dd)
 
-trajects <- dd %>%
+trajects <- distances %>%
   mutate(curr_dist = dist) %>%
   mutate(curr_pos = dst) %>%
   mutate(visited = 2^as.numeric(src) + 2^as.numeric(dst)) %>%
@@ -22,7 +22,7 @@ trajects <- dd %>%
 # Number of cities minus 2
 for (i in 3:length(city))
   trajects <- trajects %>%
-    inner_join(dd, by = c("curr_pos" = "src")) %>%
+    inner_join(distances, by = c("curr_pos" = "src")) %>%
     filter(bitwAnd(visited, 2^as.numeric(dst)) == 0) %>%
     mutate(curr_dist = curr_dist + dist) %>%
     mutate(curr_pos = dst) %>%
@@ -30,9 +30,20 @@ for (i in 3:length(city))
     select(-dst, -dist) %>%
     group_by(visited, curr_pos) %>%
     arrange(desc(curr_dist)) %>%
-    slice(1) %>%
+    slice(1) %>% # optimization to prune inefficient solutions
     ungroup
 print(trajects %>%
   arrange(desc(curr_dist)) %>%
   slice(1) %>%
   select(curr_dist))
+
+# Visualization
+library("igraph")
+# dd <- dd %>% filter(dist < 100)
+g <- graph_from_edgelist(as.matrix(dd[,c("src", "dst")]),
+                         directed = FALSE) %>%
+  set_edge_attr("label", value = dd$dist)
+# plot(g, layout = layout_with_drl(g, weights = 1 / dd$dist))
+# plot(g, layout = layout_with_fr(g, weights = 1 / dd$dist))
+# plot(g, layout = layout_with_kk(g, weights = dd$dist))
+plot(g)
