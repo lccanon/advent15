304f25a0164ca5b072bb7f89f21516ec9d584764
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-12 14:24:51 +0200

    Better solution

--- 12.R
+++ 12.R
@@ -8,18 +8,18 @@ print(json %>%
   sum)
 
 library(rjson)
+
 parsed <- json %>%
   fromJSON(simplify = FALSE)
 
 get_count <- function(tt) {
-  if (!is.null(names(tt)))
-    for (s in tt)
-      if (is.character(s) && s == "red")
+  if (!is.null(names(tt)) &&
+      any(map_lgl(tt, ~ is_character(.) && . == "red")))
         return(0)
   count <- 0
   for (s in tt)
     if (is.numeric(s))
-      count <- count + sum(s)
+      count <- count + s
     else if (is.list(s))
       count <- count + get_count(s)
     else if (!is.character(s))
