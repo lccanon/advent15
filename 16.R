7b1bde7d1ecf79d1730cb13a36caed271a9e1ec3
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-16 14:17:46 +0200

    Improvement

--- 16.R
+++ 16.R
@@ -25,25 +25,16 @@ perfumes: 1" %>%
 
 for (i in 1:nrow(const)) {
   print(nrow(aunts))
-  if (pull(const[i,1]) %in% c("cats", "trees")) {
-    aunts <- aunts %>% 
-      filter(name1 == pull(const[i,], name) & quant1 > pull(const[i,], quant) |
-               name2 == pull(const[i,], name) & quant2 > pull(const[i,], quant) |
-               name3 == pull(const[i,], name) & quant3 > pull(const[i,], quant) |
-               name1 != pull(const[i,], name) & name2 != pull(const[i,], name) & name3 != pull(const[i,], name))
-  } else if (pull(const[i,1]) %in% c("pomeranians", "goldfish")) {
-    aunts <- aunts %>% 
-      filter(name1 == pull(const[i,1]) & quant1 < pull(const[i,2]) |
-               name2 == pull(const[i,1]) & quant2 < pull(const[i,2]) |
-               name3 == pull(const[i,1]) & quant3 < pull(const[i,2]) |
-               name1 != pull(const[i,1]) & name2 != pull(const[i,1]) & name3 != pull(const[i,1]))
-  } else {
-    aunts <- aunts %>% 
-      filter(name1 == pull(const[i,1]) & quant1 == pull(const[i,2]) |
-               name2 == pull(const[i,1]) & quant2 == pull(const[i,2]) |
-               name3 == pull(const[i,1]) & quant3 == pull(const[i,2]) |
-               name1 != pull(const[i,1]) & name2 != pull(const[i,1]) & name3 != pull(const[i,1]))
-  }
+  for (j in c(2, 4, 6))
+    if (pull(const[i,1]) %in% c("cats", "trees"))
+      aunts <- aunts %>%
+        filter(aunts[,j] != pull(const[i,], name) | aunts[,j + 1] > pull(const[i,], quant))
+    else if (pull(const[i,1]) %in% c("pomeranians", "goldfish"))
+      aunts <- aunts %>%
+        filter(aunts[,j] != pull(const[i,], name) | aunts[,j + 1] < pull(const[i,], quant))
+    else
+      aunts <- aunts %>%
+        filter(aunts[,j] != pull(const[i,], name) | aunts[,j + 1] == pull(const[i,], quant))
 }
 
 aunts %>%
