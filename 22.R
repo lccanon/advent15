789731a9af719b98e592d2be7d9c106062068952
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-23 10:57:05 +0200

    Minor

--- 22.R
+++ 22.R
@@ -5,11 +5,9 @@ player_mana <- 500
 
 read_delim("input22", delim = ": ", col_names = c("property", "value")) %>%
   pivot_wider(names_from = "property", values_from = "value") -> boss
-# boss$`Hit Points` <- 14
-# boss$Damage <- 8
 
 spells <- tribble(
-  ~mana, ~damage, ~heal, ~duration_shield, ~duration_poison, ~duration_recharge,
+  ~cost, ~damage, ~heal, ~duration_shield, ~duration_poison, ~duration_recharge,
    53, 4, 0, 0, 0, 0,
    73, 2, 2, 0, 0, 0,
   113, 0, 0, 6, 0, 0,
@@ -41,9 +39,10 @@ for (i in 1:(player_hit_points + boss$`Hit Points`)) {
       # Remove losing strats
       filter(player_hit_points > 0) %>%
       # Mana is handled
-      mutate(player_mana = player_mana - mana) %>%
-      mutate(total_mana = total_mana + mana) %>%
+      mutate(player_mana = player_mana - cost) %>%
+      mutate(total_mana = total_mana + cost) %>%
       filter(player_mana >= 0) %>%
+      filter(total_mana < best_mana) %>%
       # Let's apply the selected action
       mutate(boss_hit_points = boss_hit_points - damage) %>%
       mutate(player_hit_points = player_hit_points + heal) %>%
@@ -53,7 +52,11 @@ for (i in 1:(player_hit_points + boss$`Hit Points`)) {
       select(names(strats)) %>%
       # Optimize by pruning less efficient strats
       group_by(player_hit_points, player_mana, boss_hit_points, player_armor, shield_remaining, poison_remaining, recharge_remaining) %>%
-      summarise(total_mana = min(total_mana), .groups = "drop") -> strats
+      summarise(total_mana = min(total_mana), .groups = "drop") %>%
+      group_by(total_mana, player_mana, boss_hit_points, player_armor, shield_remaining, poison_remaining, recharge_remaining) %>%
+      summarise(player_hit_points = max(player_hit_points), .groups = "drop") %>%
+      group_by(player_hit_points, total_mana, player_mana, player_armor, shield_remaining, poison_remaining, recharge_remaining) %>%
+      summarise(boss_hit_points = min(boss_hit_points), .groups = "drop") -> strats
     if (nrow(strats) == 0)
       break
     # print(nrow(strats))
