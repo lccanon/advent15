f0cc8766ebd0bf6b5f09dd600d065b57928565f0
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-29 11:36:36 +0200

    Revise assembly simulator

--- 23.R
+++ 23.R
@@ -1,43 +1,32 @@
 library(tidyverse)
 
-readLines("input23") %>%
-  strsplit("\\n") %>%
-  unlist %>%
+read_lines("input23") %>%
   str_replace(",", "") %>%
-  str_replace("([a-z])$", "\\1 0") %>%
-  str_replace("jmp", "jmp X") %>%
+  str_replace("([a-z])$", "\\1 NA") %>%
+  str_replace("jmp", "jmp NA") %>%
   tibble %>%
-  separate(col = ".", into = c("inst", "var", "val"), sep = " ") %>%
+  separate(col = ".", into = c("inst", "reg", "offset"), sep = " ") %>%
   mutate_at(3, as.integer) -> insts
 
-vars <- list()
-for (var in unique(insts$var))
-  vars[[var]] <- 0
+regs <- list()
+for (reg in unique(insts$reg))
+  regs[[reg]] <- 0
 i <- 1
-vars$a <- 1
+regs$a <- 1
 while (i <= nrow(insts)) {
-  if (insts[i,]$inst == "hlf") {
-    vars[[insts[i,]$var]] <- vars[[insts[i,]$var]] / 2
-    i <- i + 1
-  } else if (insts[i,]$inst == "tpl") {
-    vars[[insts[i,]$var]] <- vars[[insts[i,]$var]] * 3
-    i <- i + 1
-  } else if (insts[i,]$inst == "inc") {
-    vars[[insts[i,]$var]] <- vars[[insts[i,]$var]] + 1
-    i <- i + 1
-  } else if (insts[i,]$inst == "jmp") {
-    i <- i + insts[i,]$val
-  } else if (insts[i,]$inst == "jie") {
-    if (vars[[insts[i,]$var]] %% 2 == 0)
-      i <- i + insts[i,]$val
-    else
-      i <- i + 1
-  } else if (insts[i,]$inst == "jio") {
-    if (vars[[insts[i,]$var]] == 1)
-      i <- i + insts[i,]$val
-    else
-      i <- i + 1
-  } else
-    print("Problem")
+  val <- regs[[insts[i,]$reg]]
+  inst <- insts[i,]$inst
+  regs[[insts[i,]$reg]] <- case_match(inst,
+    "hlf" ~ val / 2,
+    "tpl" ~ val * 3,
+    "inc" ~ val + 1,
+    .default = val
+  )
+  i <- case_when(
+    inst == "jmp" ||
+      inst == "jie" && val %% 2 == 0 ||
+      inst == "jio" && val == 1 ~ i + insts[i,]$offset,
+    TRUE ~ i + 1,
+  )
 }
-print(vars$b)
+print(regs$b)
